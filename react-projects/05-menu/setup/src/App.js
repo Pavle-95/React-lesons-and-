f5df50d73b6238cc779cd5398f7e6d2b56711ccb
Pavle Pesic <p.pavle16@gmail.com>
2023-01-27 21:10:24 +0100

    10 project is done

--- react-projects/05-menu/setup/src/App.js
+++ react-projects/05-menu/setup/src/App.js
@@ -3,8 +3,40 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
-  return <h2>menu project setup</h2>;
+
+  const [menuItem, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+
+  const fiterItems = (category) => {
+    if(category === 'all'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+
+
+
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our mnenu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={fiterItems} categories={categories}/>
+        <Menu items={menuItem}/>
+      </section>
+    </main>
+  );
+
 }
 
 export default App;
--- react-projects/10-grocery-bud/setup/src/App.js
+++ react-projects/10-grocery-bud/setup/src/App.js
@@ -3,7 +3,116 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+
+  const getLocalStorage = () => {
+    let list = localStorage.getItem('list');
+    if(list) {
+      return JSON.parse(localStorage.getItem('list'));
+    }
+    else{
+      return [];
+    }
+  }
+
+  // States
+  const [name, setName] = useState('');
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({show: false, msg:'', type: ''});
+
+
+  // Function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!name) {
+      // Display alert
+      showAlert(true, 'danger', 'please enter the value')
+    }
+    else if (name && isEditing) {
+      // Deal with edit
+      setList(list.map((item)=> {
+        if(item.id === editID){
+          return{...item, title: name}
+        }
+        return item
+      }))
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value change')
+    }
+    else {
+      // show alert
+      showAlert(true, 'success', 'item aded to the list')
+      const newItem = {id: new Date().getTime().toString(), title: name}
+      setList([...list, newItem]);
+      setName('');
+    }
+  }
+
+  const showAlert = (show=false, type="", msg="") => {
+    setAlert({show,type,msg})
+  }
+
+  const clearList = () => {
+    showAlert(true,'success','List is clear')
+    setList([]);
+    setName('');
+    setEditID(null);
+    setIsEditing(false);
+  }
+
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed')
+    setList(list.filter((item) => item.id !== id));
+  }
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id)
+    setName(specificItem.title);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list])
+
+  return (
+    <section className='section-center'>
+      <form 
+        className='frocery-form' 
+        onSubmit={handleSubmit}
+      >
+        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input 
+            type="text" 
+            className='grocery' 
+            placeholder='e.g. eggs' 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} removeItem={removeItem} list={list} editItem={editItem}/>
+          <button 
+            className='clear-btn'
+            onClick={clearList}
+            >
+            Clear items
+          </button>
+        </div>
+      )}
+    </section>
+  )
 }
 
 export default App
